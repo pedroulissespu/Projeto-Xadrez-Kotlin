326f27e4d0c81c892a7972410ed9f2580ede9cfe
Pedro Ulisses Pereira Castro Maia <pedro.ulisses@aluno.uece.br>
2022-07-26 14:39:36 -0300

    Alteração na cor do tabuleiro

--- Xadrez/app/src/main/java/com/game/xadrez/ModeloXadrez.kt
+++ Xadrez/app/src/main/java/com/game/xadrez/ModeloXadrez.kt
@@ -1,83 +1,81 @@
 package com.game.xadrez
 
-class ModeloXadrez {
-    var espacopeca = mutableSetOf<PecaXadrez>()
+import android.view.View
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.Display
+
+class MostraArena(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+    private final val cellSide: Float = 130f
+    private final val originX = 20f
+    private final val originY = 200f
+    private final val corBranca = Color.argb(1f,.4f,.4f,.4f)
+    private final val corPreta = Color.argb(1f,.5f,.5f,.5f)
+    private final val imgResIDs = setOf(
+        R.drawable.chess_rdt60,
+        R.drawable.chess_rlt60,
+        R.drawable.chess_qlt60,
+        R.drawable.chess_qdt60,
+        R.drawable.chess_plt60,
+        R.drawable.chess_pdt60,
+        R.drawable.chess_nlt60,
+        R.drawable.chess_ndt60,
+        R.drawable.chess_klt60,
+        R.drawable.chess_kdt60,
+        R.drawable.chess_blt60,
+        R.drawable.chess_bdt60
+    )
+    private final val bitmaps = mutableMapOf<Int, Bitmap>()
+    private final val paint = Paint()
 
     init{
-        reset()
+        carregaBitmaps()
     }
 
-    fun reset(){
-        espacopeca.removeAll(espacopeca)
-        for(i in 0..1) {
-            espacopeca.add(PecaXadrez(0+i*7, 0, Player.WHITE, Rank.TORRE,R.drawable.chess_rlt60))
-            espacopeca.add(PecaXadrez(0+i*7, 7, Player.BLACK, Rank.TORRE,R.drawable.chess_rdt60))
-
-            espacopeca.add(PecaXadrez(1+i*5, 0, Player.WHITE, Rank.CAVALO,R.drawable.chess_nlt60))
-            espacopeca.add(PecaXadrez(1+i*5, 7, Player.BLACK, Rank.CAVALO,R.drawable.chess_ndt60))
-
-            espacopeca.add(PecaXadrez(2+i*3, 0, Player.WHITE, Rank.BISPO,R.drawable.chess_blt60))
-            espacopeca.add(PecaXadrez(2+i*3, 7, Player.BLACK, Rank.BISPO,R.drawable.chess_bdt60))
-        }
-
-        for(i in 0..7){
-            espacopeca.add(PecaXadrez(i, 1, Player.WHITE, Rank.PEÃO,R.drawable.chess_plt60))
-            espacopeca.add(PecaXadrez(i, 6, Player.BLACK, Rank.PEÃO,R.drawable.chess_pdt60))
-        }
-
-        espacopeca.add(PecaXadrez(3, 0, Player.WHITE, Rank.RAINHA,R.drawable.chess_qlt60))
-        espacopeca.add(PecaXadrez(3, 7, Player.BLACK, Rank.RAINHA,R.drawable.chess_qdt60))
-
-        espacopeca.add(PecaXadrez(4, 0, Player.WHITE, Rank.REI,R.drawable.chess_klt60))
-        espacopeca.add(PecaXadrez(4, 7, Player.BLACK, Rank.REI,R.drawable.chess_kdt60))
+    override fun onDraw(canvas: Canvas?){
+        desenhaTabuleiroXadrez(canvas)
+        desenhaPecas(canvas)
     }
 
-    fun pieceAt(col: Int , row : Int) : PecaXadrez?{
-        for(piece in espacopeca){
-            if(col == piece.col && row == piece.row){
-                return piece
-            }
-        }
-        return null
-    }
+    private fun desenhaPecas(canvas: Canvas?){
+        val modeloXadrez = ModeloXadrez()
+        modeloXadrez.reset()
 
-    override fun toString(): String{
-        var descricao = " \n"
-        for(row in 7 downTo 0){
-            val r = 7-row
-            descricao += "$r"
-            for(col in 0..7){
-                val piece = pieceAt(col,row)
-                if(piece == null) {
-                    descricao += " ."
-                } else{
-                    val white = piece.player == Player.WHITE
-                    descricao += " "
-                    descricao += when(piece.rank) {
-                        Rank.REI -> {
-                            if(white) "k" else "K"
-                        }
-                        Rank.RAINHA -> {
-                            if(white) "q" else "Q"
-                        }
-                        Rank.BISPO -> {
-                            if(white) "b" else "B"
-                        }
-                        Rank.TORRE -> {
-                            if(white) "t" else "T"
-                        }
-                        Rank.CAVALO -> {
-                            if(white) "c" else "C"
-                        }
-                        Rank.PEÃO -> {
-                            if(white) "p" else "P"
-                        }
-                    }
+        for (row in 0..7){
+            for (col in 0..7){
+                val piece = modeloXadrez.pieceAt(col,row)
+                if(piece != null){
+                    drawPiecesAt(canvas, col, row, piece.resID)
                 }
             }
-            descricao+= "\n"
         }
-        descricao += " 0 1 2 3 4 5 6 7"
-        return descricao
+    }
+
+    private fun drawPiecesAt(canvas : Canvas? , col: Int , row : Int , resID: Int){
+        val bitmap = bitmaps[resID]!!
+        canvas?.drawBitmap(bitmap, null, RectF(originX + col * cellSide,originY + (7 - row) * cellSide,originX + (col + 1) * cellSide,originY + ((7 - row) + 1) * cellSide),paint)
+    }
+
+    private fun carregaBitmaps(){
+        imgResIDs.forEach{
+            bitmaps[it] = BitmapFactory.decodeResource(resources,it)
+        }
+    }
+
+    private fun desenhaTabuleiroXadrez(canvas: Canvas?){
+        val paint = Paint()
+        for(i in 0..7) {
+            for (j in 0..7) {
+                paint.color = if((i + j) % 2 == 1) corPreta else corBranca
+                canvas?.drawRect(
+                    originX + i * cellSide,
+                    originY + j * cellSide,
+                    originX + (i + 1) * cellSide,
+                    originY + (j + 1) * cellSide,
+                    paint
+                )
+            }
+        }
     }
 }
