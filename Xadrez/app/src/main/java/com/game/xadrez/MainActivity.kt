b687b24a2fc0fca09f3b4f6193cd7f4138f1620f
Pedro Ulisses Pereira Castro Maia <pedro.ulisses@aluno.uece.br>
2022-07-26 15:10:02 -0300

    Finalizado em partes , fase de testes

--- Xadrez/app/src/main/java/com/game/xadrez/MainActivity.kt
+++ Xadrez/app/src/main/java/com/game/xadrez/MainActivity.kt
@@ -16,13 +16,90 @@ import java.util.concurrent.Executors
 
 private const val TAG = "MainActivity"
 
-class MainActivity : AppCompatActivity() {
-
-    var modeloxadrez = ModeloXadrez()
+class MainActivity : AppCompatActivity(), ChessDelegate {
+    private val socketHost = "127.0.0.1"
+    private val socketPort: Int = 50000
+    private val socketGuestPort: Int = 50001 // used for socket server on emulator
+    private lateinit var chessView: ChessView
+    private lateinit var resetButton: Button
+    private lateinit var listenButton: Button
+    private lateinit var connectButton: Button
+    private var printWriter: PrintWriter? = null
+    private var serverSocket: ServerSocket? = null
+    private val isEmulator = Build.FINGERPRINT.contains("generic")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "$modeloxadrez")
+
+        chessView = findViewById<ChessView>(R.id.chess_view)
+        resetButton = findViewById<Button>(R.id.reset_button)
+        listenButton = findViewById<Button>(R.id.listen_button)
+        connectButton = findViewById<Button>(R.id.connect_button)
+        chessView.chessDelegate = this
+
+        resetButton.setOnClickListener {
+            ChessGame.reset()
+            chessView.invalidate()
+            serverSocket?.close()
+            listenButton.isEnabled = true
+        }
+
+        listenButton.setOnClickListener {
+            listenButton.isEnabled = false
+            val port = if (isEmulator) socketGuestPort else socketPort
+            Toast.makeText(this, "listening on $port", Toast.LENGTH_SHORT).show()
+            Executors.newSingleThreadExecutor().execute {
+                ServerSocket(port).let { srvSkt ->
+                    serverSocket = srvSkt
+                    try {
+                        val socket = srvSkt.accept()
+                        receiveMove(socket)
+                    } catch (e: SocketException) {
+                        // ignored, socket closed
+                    }
+                }
+            }
+        }
+
+        connectButton.setOnClickListener {
+            Log.d(TAG, "socket client connecting ...")
+            Executors.newSingleThreadExecutor().execute {
+                try {
+                    val socket = Socket(socketHost, socketPort)
+                    receiveMove(socket)
+                } catch (e: ConnectException) {
+                    runOnUiThread {
+                        Toast.makeText(this, "connection failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun receiveMove(socket: Socket) {
+        val scanner = Scanner(socket.getInputStream())
+        printWriter = PrintWriter(socket.getOutputStream(), true)
+        while (scanner.hasNextLine()) {
+            val move = scanner.nextLine().split(",").map { it.toInt() }
+            runOnUiThread {
+                ChessGame.movePiece(Square(move[0], move[1]), Square(move[2], move[3]))
+                chessView.invalidate()
+            }
+        }
+    }
+
+    override fun pieceAt(square: Square): ChessPiece? = ChessGame.pieceAt(square)
+
+    override fun movePiece(from: Square, to: Square) {
+        ChessGame.movePiece(from, to)
+        chessView.invalidate()
+
+        printWriter?.let {
+            val moveStr = "${from.col},${from.row},${to.col},${to.row}"
+            Executors.newSingleThreadExecutor().execute {
+                it.println(moveStr)
+            }
+        }
     }
 }
